bfb96a5635c31442a956bf0ac665767ce036b0e4
Yeison Osorio <yeison-arley25@hotmail.com>
2015-11-04 21:22:54 +0000

    creando datos de prueba

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Bar.create!(nombre: 'bar1', direccion: 'calle1', telefono: '1234567', publicidad: 'hola')
+Bar.create!(nombre: 'bar2', direccion: 'calle2', telefono: '1234567', publicidad: 'hola')
+Bar.create!(nombre: 'bar3', direccion: 'calle3', telefono: '1234567', publicidad: 'hola')
+Bar.create!(nombre: 'bar4', direccion: 'calle4', telefono: '1234567', publicidad: 'hola')
+Bar.create!(nombre: 'bar5', direccion: 'calle5', telefono: '1234567', publicidad: 'hola')
