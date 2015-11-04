5e5c7256e4c2756a8bdbdc9be5d1dae945c393b3
Yeison Osorio <yeison-arley25@hotmail.com>
2015-11-04 22:43:53 +0000

    obtener datos en json desde el controlador

--- app/controllers/api/bares_controller.rb
+++ app/controllers/api/bares_controller.rb
@@ -1,2 +1,15 @@
 class Api::BaresController < ApplicationController
+  respond_to :json
+  PER_PAGE_RECORDS = 9
+  #GET /user.json
+  def index
+    bares_paginated = Bar.order('id').page(params[:page]).per(PER_PAGE_RECORDS)
+    json_response = {
+      models: bares_paginated,
+      current_page: params[:page].to_i,
+      perPage: PER_PAGE_RECORDS,
+      total_pages: bares_paginated.num_pages
+    }
+    respond_with json_response, callback: params[:callback]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   namespace :api do
-    resources :bares
+    resources :bares, format: :json
   end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
